d477a7d6c7d48b68520c2fe1ce92a903b42e4ab1
Val Kadalov <vkadalov@gmail.com>
2025-12-11 03:32:33 +0000

    OOP-based file created basic-oop.py
    Created file inteded for OOP and given comments to clarify the purpose

---
+++ basic-oop.py
@@ -0,0 +1,3 @@
+### In this file there will be constant use of OOP techniques such as inheritence, polymorphism and others ###
+### All collaborators will be working in this file only intended for its purpose ###
+
